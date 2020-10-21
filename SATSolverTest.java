6cc7ed9ad1e3788f5ad3d29c161838efdca33852
JibrilTheUltimateFlugel <73127262+JibrilTheUltimateFlugel@users.noreply.github.com>
2020-10-21 12:58:05 +0800

    Update SATSolverTest.java

--- SATSolverTest.java
+++ SATSolverTest.java
@@ -39,12 +39,11 @@ public class SATSolverTest {
         if (e == null) {
             System.out.println("not satisfiable");
         } else{
-            System.out.println("not satisfiable");
+            System.out.println("satisfiable");
             System.out.println(e);}
     }
 
 
-    // TODO: add the main method that reads the .cnf file and calls SATSolver.solve to determine the satisfiability
     public static Formula readFile(String filename) throws IOException { //very important method to read the file
         Scanner scans = null; //creates scanner object with initial value null
         boolean existsProblem = false; //boolean variable to indicate whether there is a problem to solve or not
@@ -53,31 +52,29 @@ public class SATSolverTest {
         try {
             scans = new Scanner(new File(filename)); //have to handle file not found exception
             String inputstring = null;
-
-            while (scans.hasNextLine()) {
-                inputstring = scans.nextLine();
+            while (scans.hasNextLine()) { //while the scanner object is still able to scan the next line
+                inputstring = scans.nextLine(); //create a string from the nextLine scanned by the scanner
                 if (inputstring.isEmpty() || inputstring.charAt(0) == 'c') { //if the line is empty or the line is a comment line then skip it
                     continue;
                 }
                 if (inputstring.charAt(0) == 'p') { //if the first character of the line/string is p means its a problem
-                    existsProblem = true;
-                    String[] listofstring = inputstring.split(" ");
+                    String[] listofstring = inputstring.split(" "); //convert the line into a list of strings divided based on spaces
                     Integer indextoplace = Integer.parseInt(listofstring[listofstring.length - 1]);
                     listofclauses = new Clause[indextoplace];
                     pointerClause = indextoplace - 1;
+                    existsProblem = true; //then there exists a problem
                     break;
                 }
-
             }
-
-            if (existsProblem == true) {
+            if (existsProblem == true) { //if there exists a problem
                 scans.useDelimiter(" 0");
-                while (scans.hasNext()) {
-                    String nextstring = scans.next();
+                while (scans.hasNext()) { //while the scanner still can scan the next string
+                    String nextstring;
+                    nextstring = scans.next();
                     String[] listofvalues = nextstring.trim().split(" ");
                     if (listofvalues.length > 2) { //if the clause has more than 2 "values" or in this case literals,
                         a2SATproblem = false;
-                        throw new IOException("More than 2 literals in one clause.");
+                        //throw new IOException("More than 2 literals in one clause.");
                     }
                     Literal[] listofliterals = new Literal[listofvalues.length];
                     for (int k = 0; k < listofliterals.length; k++) {
@@ -87,29 +84,30 @@ public class SATSolverTest {
                         }
                     }
 
-                    if (listofliterals[0] != null) {
-                        listofclauses[pointerClause] = makeCl(listofliterals);
+                    if (listofliterals[0] != null) { //if the first element of the list of literals is not null
+                        listofclauses[pointerClause] = makeCl(listofliterals); //make a new entry in the listofclauses with index pointerClause
                         if (listofclauses[pointerClause] == null)
-                            throw new IOException("Unsatisfiable");
+                            throw new IOException("unsatisfiable");
                     }
                     pointerClause--;
                 }
             } else {
-                throw new IOException("no P or problem found");
+                throw new IOException("no problem found");
             }
-
-
         } catch (FileNotFoundException eksepsi) {
-            eksepsi.printStackTrace();
+            System.out.println("File not found exception");
+            System.exit(0);
         } catch (ArrayIndexOutOfBoundsException eksepsi) {
             System.out.println("CNF file contains more clause than expected");
+            System.exit(0);
         } catch (IOException eksepsi) {
-            eksepsi.printStackTrace();
-        } finally {
-            scans.close();
+            System.out.println("I/O Exception");
+            System.exit(0);
+        }
+        finally {
+            scans.close(); //closes the scanner object
         }
         return makeFm(listofclauses);
-
     }
 
 
@@ -133,9 +131,6 @@ public class SATSolverTest {
 */
     }
 
-    //class to read the whole file
-
-
     private static Formula makeFm(Clause... e) {
         Formula f = new Formula();
         for (Clause c : e) {
@@ -154,21 +149,19 @@ public class SATSolverTest {
 
     public static void main(String args[]) throws IOException {
         if (args.length > 0) {
-            String filename = args[0];
-            SATSolverTest testings = new SATSolverTest();
-            Formula formulatest = readFile(filename);
-            if (isa2SATproblem()) {
+            String filename = args[0]; //the filename supplied will be the first element of the args array
+            SATSolverTest testings = new SATSolverTest(); //create new instance of the SATSolverTest class
+            Formula formulatest = readFile(filename); //creates formula called formulatest by utilizing the readFile method on file specified by filename
+            if (isa2SATproblem()) { //if the problem is a 2 literal SAT problem
                 testings.startSATSolver(formulatest);
-            } else if(isa2SATproblem()==false) {
+            } else if(isa2SATproblem()==false) { //if the problem is not a 2 literal SAT problem
                 System.out.println("More than 2 literals");
-                System.exit(1);
+                testings.startSATSolver(formulatest);
             }
             else{
-                System.out.println("Supply the location of the file. Syntax: solver.jar file");
-                System.exit(1);
+                System.out.println("Supply the location of the file");
+                System.exit(0);
             }
         }
-
-
     }
 }
